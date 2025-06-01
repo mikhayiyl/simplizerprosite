4b6c45bbb23677bccf38a250ca8184b916899847
Dancan <duhyncan@gmail.com>
2025-06-01 19:23:28 +0300

    Add lazy loading to App.tsx using React.lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Hero from "./components/Hero";
+import React, { Suspense } from "react";
 import Navbar from "./components/Navbar";
-import Services from "./components/Services";
 
-function App() {
+const Hero = React.lazy(() => import("./components/Hero"));
+const About = React.lazy(() => import("./components/About"));
+const Services = React.lazy(() => import("./components/Services"));
+const Contact = React.lazy(() => import("./components/Contact"));
+
+export default function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <About />
-      <Services />
-      <Contact />
+      <main
+        className="bg-white dark:bg-black overflow-x-hidden scroll-smooth"
+        role="main"
+        tabIndex={0} // Improves keyboard accessibility
+      >
+        <Suspense fallback={<div>Loading...</div>}>
+          <Hero />
+          <About />
+          <Services />
+          <Contact />
+        </Suspense>
+      </main>
     </>
   );
 }
-
-export default App;
